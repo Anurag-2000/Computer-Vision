5a2f6249c721bce3ead966c9c3ee476c675df5b9
Anurag-2000 <anurag3sharma4@gmail.com>
2021-08-19 07:30:12 +0530

    init_chap3

---
+++ chapter_3.py
@@ -0,0 +1,19 @@
+# Resize and croping
+
+import cv2
+import numpy as np
+ 
+img = cv2.imread(r'F:\\pycodes\\opencv\\pics\s.jpg')
+
+# print(img.shape)
+ 
+imgResize = cv2.resize(img,(100,50))
+# print(imgResize.shape)
+ 
+imgCropped = img[0:200,200:500]
+ 
+cv2.imshow("Image",img)
+cv2.imshow("Image Resize",imgResize)
+cv2.imshow("Image Cropped",imgCropped)
+ 
+cv2.waitKey(0)
